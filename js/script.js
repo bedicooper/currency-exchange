1f00e34b6b8bfa68990a30cc05449750e7702342
Mateusz Bednarz <bednarz.matuesz@gmail.com>
2022-11-21 14:18:04 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let formElement = document.querySelector(".js-form");
+let amountFromElement = document.querySelector(".js-amountFrom");
+let currencyFromElement = document.querySelector(".js-currencyFrom");
+let currencyToElement = document.querySelector(".js-currencyTo");
+let resultElement = document.querySelector(".js-result");
+let resultCurrencyElement = document.querySelector(".js-resultCurrency");
+let resultPhraseElement = document.querySelector(".js-resultPhrase");
+
+console.log(currencyToElement);
+console.log(resultCurrencyElement);
+
+
+// jak do tych zmiennych pobrać dane z google? ;)
+let PLN = 1.00;
+let GBP = 5.48;
+let USD = 4.72;
+let EUR = 4.72;
+let CHF = 4.77;
+let JPY = 0.032;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amountFrom = amountFromElement.value;
+    let currencyFrom
+    let currencyTo
+    let exchangeCalculation
+    let resultPhrase
+
+    switch (currencyFromElement.value) {
+        case "PLN":
+            currencyFrom = PLN;
+            break;
+        case "USD":
+            currencyFrom = USD;
+            break;
+        case "EUR":
+            currencyFrom = EUR;
+            break;
+        case "GBP":
+            currencyFrom = GBP;
+            break;
+        case "CHF":
+            currencyFrom = CHF;
+            break;
+        case "JPY":
+            currencyFrom = JPY;
+            break;
+    };
+
+    switch (currencyToElement.value) {
+        case "PLN":
+            currencyTo = PLN;
+            break;
+        case "USD":
+            currencyTo = USD;
+            break;
+        case "EUR":
+            currencyTo = EUR;
+            break;
+        case "GBP":
+            currencyTo = GBP;
+            break;
+        case "CHF":
+            currencyTo = CHF;
+            break;
+        case "JPY":
+            currencyTo = JPY;
+            break;
+    };
+
+    exchangeCalculation = (amountFrom * currencyFrom) / currencyTo;
+
+    resultPhrase = `${amountFrom} ${currencyFromElement.value} to ${exchangeCalculation.toFixed(2)} ${currencyToElement.value}`;
+
+    resultPhraseElement.innerText = resultPhrase;
+});
