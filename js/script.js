68989f5aa3a722102072e4ab8bac8c9df6bfb6b9
Mateusz Bednarz <bednarz.matuesz@gmail.com>
2022-11-24 16:51:48 +0100

    changed exchangeCaluclation into a function

--- js/script.js
+++ js/script.js
@@ -1,103 +1,91 @@
 {
-let formElement = document.querySelector(".js-form");
-let amountFromElement = document.querySelector(".js-amountFrom");
-let currencyFromElement = document.querySelector(".js-currencyFrom");
-let currencyToElement = document.querySelector(".js-currencyTo");
-let resultElement = document.querySelector(".js-result");
-let resultCurrencyElement = document.querySelector(".js-resultCurrency");
+    const ratePLN = 1.00;
+    const rateGBP = 5.48;
+    const rateUSD = 4.72;
+    const rateEUR = 4.72;
+    const rateCHF = 4.77;
+    const rateJPY = 0.032;
 
+    const calculateFromValue = (amountFrom, currencyFrom) => {
+        switch (currencyFrom) {
+            case "PLN":
+                return amountFrom * ratePLN;
 
-let PLN = 1.00;
-let GBP = 5.48;
-let USD = 4.72;
-let EUR = 4.72;
-let CHF = 4.77;
-let JPY = 0.032;
+            case "USD":
+                return amountFrom * rateUSD;
 
-const ratePLN = 1.00;
-const rateGBP = 5.48;
-const rateUSD = 4.72;
-const rateEUR = 4.72;
-const rateCHF = 4.77;
-const rateJPY = 0.032;
+            case "EUR":
+                return amountFrom * rateEUR;
 
+            case "GBP":
+                return amountFrom * rateGBP;
 
-const calculateFromValue = (amountFrom, currencyFrom) => {
-switch (currencyFrom) {
-    case "PLN":
-      return amountFrom * ratePLN;
-       
-    case "USD":
-        return amountFrom * rateUSD;
-       
-    case "EUR":
-        return amountFrom * rateEUR;
-       
-    case "GBP":
-        return amountFrom * rateGBP;
-        
-    case "CHF":
-        return amountFrom * rateCHF;
-        
-    case "JPY":
-        return amountFrom * rateJPY;
-       
-};
-};
+            case "CHF":
+                return amountFrom * rateCHF;
 
+            case "JPY":
+                return amountFrom * rateJPY;
 
-
-const updateResultText = (amountFrom, currencyFromElement, exchangeCalculation, currencyToElement) => {
-    const resultPhraseElement = document.querySelector(".js-resultPhrase");
-    resultPhraseElement.innerText = `${amountFrom} ${currencyFromElement.value} to ${exchangeCalculation.toFixed(2)} ${currencyToElement.value}`;
-};
-
-
-const onFormSubmit = (event) => {
-    event.preventDefault();
-
-    const amountFromElement = document.querySelector(".js-amountFrom");
-    const currencyFromElement = document.querySelector(".js-currencyFrom");
-    const currencyToElement = document.querySelector(".js-currencyTo");
-    
-    const amountFrom = +amountFromElement.value; 
-    const currencyFrom = currencyFromElement.value;
-    const currencyTo = currencyToElement.value;
-    
-
-    switch (currencyTo) {
-        case "PLN":
-            toRate = PLN;
-            break;
-        case "USD":
-            toRate = USD;
-            break;
-        case "EUR":
-            toRate = EUR;
-            break;
-        case "GBP":
-            toRate = GBP;
-            break;
-        case "CHF":
-            toRate = CHF;
-            break;
-        case "JPY":
-            toRate = JPY;
-            break;
+        };
     };
 
+    const calculateToRate = (currencyTo) => {
+        let rateTo
+        switch (currencyTo) {
+            case "PLN":
+                rateTo = ratePLN;
+                break;
+            case "USD":
+                rateTo = rateUSD;
+                break;
+            case "EUR":
+                rateTo = rateEUR;
+                break;
+            case "GBP":
+                rateTo = rateGBP;
+                break;
+            case "CHF":
+                rateTo = rateCHF;
+                break;
+            case "JPY":
+                rateTo = rateJPY;
+                break;
+        };
+        return rateTo;
+    };
 
-const fromValue = calculateFromValue (amountFrom, currencyFrom);
-const exchangeCalculation = fromValue / toRate;
+    const exchangeCalculation = (fromValue, toRate) => {
+        return fromValue / toRate;
+    }
 
-    updateResultText(amountFrom, currencyFromElement, exchangeCalculation, currencyToElement);
-};
+    const updateResultText = (amountFrom, currencyFrom, result, currencyTo) => {
+        const resultPhraseElement = document.querySelector(".js-resultPhrase");
+        resultPhraseElement.innerText = `${amountFrom} ${currencyFrom} to ${result.toFixed(2)} ${currencyTo}`;
+    };
 
-const init = () => {
-    const formElement = document.querySelector(".js-form");
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const amountFromElement = document.querySelector(".js-amountFrom");
+        const currencyFromElement = document.querySelector(".js-currencyFrom");
+        const currencyToElement = document.querySelector(".js-currencyTo");
 
-    formElement.addEventListener("submit", onFormSubmit);
-};
+        const amountFrom = +amountFromElement.value;
+        const currencyFrom = currencyFromElement.value;
+        const currencyTo = currencyToElement.value;
 
-init();
+        const fromValue = calculateFromValue(amountFrom, currencyFrom);
+        const toRate = calculateToRate(currencyTo);
+
+        const result = exchangeCalculation(fromValue, toRate);
+
+        updateResultText(amountFrom, currencyFrom, result, currencyTo);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
 }
