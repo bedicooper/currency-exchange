f5c5adef0ea914acbbfec72308499d0831d53b8a
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-10-27 21:41:30 +0200

    props of currencies modified and new added

--- src/currencies/index.js
+++ src/currencies/index.js
@@ -1,8 +1,8 @@
 export const currencies = [
-    { id: 1, name: 'PLN', rate: 1.0, },
-    { id: 2, name: 'GBP', rate: 5.13, },
-    { id: 3, name: 'USD', rate: 4.23, },
-    { id: 4, name: 'EUR', rate: 4.46, },
-    { id: 5, name: 'CHF', rate: 4.72, },
-    { id: 6, name: 'JPY', rate: 0.028, },
+    { id: 1, code: 'PLN', name: 'polski złoty', rate: 1.0, },
+    { id: 2, code: 'GBP', name: 'funt brytyjski', rate: 5.13, },
+    { id: 3, code: 'USD', name: 'dolar amerykański', rate: 4.23, },
+    { id: 4, code: 'EUR', name: 'euro', rate: 4.46, },
+    { id: 5, code: 'CHF', name: 'frank szwajcarski', rate: 4.72, },
+    { id: 6, code: 'JPY', name: 'jen japoński', rate: 0.028, },
 ];
