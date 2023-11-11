267c94579c60ff1f795d784096723d27bef43293
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-11 14:43:41 +0100

    request url updated

--- src/useGetRatesData.js
+++ src/useGetRatesData.js
@@ -32,7 +32,7 @@ export const useGetRatesData = () => {
   const requestRates = () => {
     (async () => {
       try {
-        const response = await axios.get("currency-exchange/data.json");
+        const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_TxBZVPlw4Ri6EYX86I67OnfSkC8GetVi56sXTCce&currencies=PLN%2CGBP%2CUSD%2CEUR%2CCHF%2CJPY&base_currency=PLN");
         updateRequestStatus(response.data);
       } catch (error) {
         updateErrorStatus(error);
