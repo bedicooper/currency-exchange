95585d52b5e681633a6c290b6adab4e00a77f0fc
Mateusz Bednarz <bednarz.mateusz@gmail.com>
2023-11-11 20:53:57 +0100

    saving data to localStorage to minimize outgoing request

--- src/useGetRatesData.js
+++ src/useGetRatesData.js
@@ -33,6 +33,7 @@ export const useGetRatesData = () => {
     (async () => {
       try {
         const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_TxBZVPlw4Ri6EYX86I67OnfSkC8GetVi56sXTCce&currencies=PLN%2CGBP%2CUSD%2CEUR%2CCHF%2CJPY&base_currency=PLN");
+        localStorage.setItem("rates", JSON.stringify(response.data));
         updateRequestStatus(response.data);
       } catch (error) {
         updateErrorStatus(error);
@@ -40,10 +41,38 @@ export const useGetRatesData = () => {
     })();
   };
 
+  const getInitialState = (requestDelay) => {
+    const localStorageState = localStorage.getItem("rates");
+
+    const dateDifference = () => {
+      const initialState = JSON.parse(localStorageState);
+      const storedIsoDate = initialState.meta.last_updated_at;
+      const storedDate = new Date(storedIsoDate);
+      const daysDifference = (Date.now() - storedDate.getTime()) / 1000 / 3600 / 24;
+
+      return daysDifference;
+    };
+
+    if (localStorageState === null) {
+      console.log("Storage empty - Request Data form API");
+      requestDelay();
+    } else if (Math.floor(dateDifference()) !== 0) {
+      console.log("To old data  - Request Data form API");
+      requestDelay();
+    } else {
+      console.log("Rates up to date - Get Data from localStorage");
+      updateRequestStatus(JSON.parse(localStorageState));
+    }
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      requestRates();
-    }, 1500);
+    const requestDelay = () => {
+      setTimeout(() => {
+        requestRates();
+      }, 1500);
+    }
+
+    getInitialState(requestDelay);
   }, []);
 
   return ratesData;
